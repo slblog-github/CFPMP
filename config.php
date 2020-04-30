156ee4e3ac9b3f0f095faad98ee77fb10d7e5807
slblog-github <64446074+slblog-github@users.noreply.github.com>
2020-04-30 08:30:24 +0800

    Update config.php

--- config.php
+++ config.php
@@ -7,9 +7,9 @@ define("HOST_KEY","");
 define("SITE_NAME","SL Domain Dns");
 
 // reCAPTCHA配置
-define("Enable_reCAPTCHA",false);
-define("reCAPTCHA_Site","");
-define("reCAPTCHA_Secret","");
+define("Enable_reCAPTCHA",true);
+define("reCAPTCHA_Site","6LchQ_AUAAAAAGOCE5EiA7KMLx443sX-5nVHMpl_");
+define("reCAPTCHA_Secret","6LchQ_AUAAAAAG_EH4_Yg7WroLM_k3WJANq-W-RW");
 
 //A记录解析（基于sslip.io）
 define("Enable_A_Record",true);
